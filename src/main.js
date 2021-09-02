c56ceafe15eb33481d291e56495fb19eeb1d5c4d
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-09-02 20:09:36 +0700

    Added files

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
+import router from './router'
+import Index from './Index'
+//import walletManager from "./walletManager";
+
+import 'bootstrap'
+
+Vue.router = router;
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+// Vue.use(walletManager);
+
+// Vue.component('main-component', );
+import punks from './punks'
+import punks_attributes from './attributes'
+
+window.punks = punks;
+window.punks_attributes = punks_attributes;
+if (window.location.href.indexOf('crosspunks.com') > -1 || window.location.href.indexOf('ost:') > -1)
+    new Vue({
+        render: h => h(Index),
+        el: '#app',
+        router,
+    });
