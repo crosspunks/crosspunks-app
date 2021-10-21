251246d1496ddd6b9e8a985d15949375e890d619
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-10-21 17:23:05 +0700

    Added video

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -50,16 +50,9 @@
             </div> -->
             <div style="height: 800px" class="crosspunk-block crosspunk-how-block">
                 <div class="crosspunk-image bg-how-image"></div>
-                <div class="crosspunk-text container bg-how-text">
+                <div class="crosspunk-text container bg-how-text" style="height: 100%;">
                     <h2 class="crosspunk-h1">How to mint</h2>
-                    <p>1) Install Metamask Extension to your favourite browser <a target="_blank" href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">Chrome</a> or <a target="_blank" href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/">Firefox</a></p>
-                    <p>2) <a target="_blank" href="https://docs.binance.org/smart-chain/wallet/metamask.html">Create your Metamask wallet</a></p>
-                    <p>3) Go to <a href="/#/">CrossPunks.com</a> and your Metamask will ask you to connect to Binance Smart Chain network. Approve it</p>
-                    <p>4) Copy your BNB wallet adress from Metamask. Go to <a target="_blank" href="https://binance.com/">Binance.com</a> (make sure you're on a real site with an SSL certificate)</p>
-                    <p>5) Top-Up your account (using crypto or fiat methods). Here you may find a guidline <a target="_blank" href="https://www.binance.com/en/buy-BNB">how to buy BNB</a></p>
-                    <p>6) Send BNB from your Binance account to your Metamask wallet. (check twice you're entering the right wallet) and take into account a small fee Binance Smart Chain will take (around 1c)</p>
-                    <p>7) Press the <router-link :to="{ name : 'mint' }">Mint</router-link> button</p>
-                    <p>8) Wait an enjoy your freshly-baked CrossPunk</p>
+                    <iframe width="100%" height="66%" src="https://www.youtube.com/embed/XuXulaydN9k?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
             <div style="height: 530px" class="crosspunk-block crosspunk-what-block">
